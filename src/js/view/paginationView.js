7980df07735be6d7da51ff6fedbc056aca114830
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2022-06-28 17:16:16 +0700

    _generateMarkupButton()

--- src/js/view/paginationView.js
+++ src/js/view/paginationView.js
@@ -17,60 +17,56 @@ class paginationView extends View {
   }
 
   _generateMarkup() {
+    const curPage = this._data.page;
     const totalPages = Math.ceil(
       this._data.results / this._data.resultsPerPage
     );
 
     // Only 1 page
     if (totalPages === 1) return '';
-
     // 1st page, others
-    if (this._data.page === 1) {
-      return `
-        <button data-go-to="${
-          this._data.page + 1
-        }" class="btn--inline pagination__btn pagination__btn--next">
-          <span>Page ${this._data.page + 1}</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </button>
-      `;
-    }
-
+    if (this._data.page === 1)
+      return this._generateMarkupButton(curPage, 'next');
     // Last page
-    if (this._data.page === totalPages) {
-      return `
-        <button data-go-to="${
-          this._data.page - 1
-        }" class="btn--inline pagination__btn pagination__btn--prev">
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-left"></use>
-          </svg>
-          <span>Page ${this._data.page - 1}</span>
-        </button>
-      `;
-    }
-
+    if (this._data.page === totalPages)
+      return this._generateMarkupButton(curPage, 'prev');
     // Middle page
-    return `
+    return this._generateMarkupButton(curPage, 'both');
+  }
+
+  _generateMarkupButton(curPage, direction) {
+    if (direction === 'prev') {
+      return `
       <button data-go-to="${
-        this._data.page - 1
+        curPage - 1
       }" class="btn--inline pagination__btn pagination__btn--prev">
         <svg class="search__icon">
           <use href="${icons}#icon-arrow-left"></use>
         </svg>
-        <span>Page ${this._data.page - 1}</span>
+      <span>Page ${curPage - 1}</span>
       </button>
+    `;
+    }
+
+    if (direction === 'next') {
+      return `
       <button data-go-to="${
-        this._data.page + 1
+        curPage + 1
       }" class="btn--inline pagination__btn pagination__btn--next">
-        <span>Page ${this._data.page + 1}</span>
+      <span>Page ${curPage + 1}</span>
         <svg class="search__icon">
           <use href="${icons}#icon-arrow-right"></use>
         </svg>
       </button>
     `;
+    }
+
+    if (direction === 'both') {
+      return (
+        this._generateMarkupButton(curPage, 'prev') +
+        this._generateMarkupButton(curPage, 'next')
+      );
+    }
   }
 }
 
